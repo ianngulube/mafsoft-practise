d6524099cdc93737ec2421c3556051decfc01074
Ian Ngulube <ngulubeian94@gmail.com>
2024-03-28 12:09:31 +0200

    resources:add resource class

---
+++ src/main/java/za/co/mafsoft/fundamentals/resources/MoviesResource.java
@@ -0,0 +1,60 @@
+package za.co.mafsoft.fundamentals.resources;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import za.co.mafsoft.fundamentals.client.TvMazeApi;
+import za.co.mafsoft.fundamentals.model.Movie;
+import za.co.mafsoft.fundamentals.model.TvMazeSeries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/movies")
+@Tag(name = "Movies Resource", description = "API to create, read, update and delete movies collection")
+public class MoviesResource {
+
+    @RestClient
+    private TvMazeApi tvMazeApi;
+
+    private final List<Movie> movies = new ArrayList<>(5);
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get all the movies",
+            description = "This will get all the movies in the database")
+    public Response getAllMovies() {
+        return Response.ok(movies).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/size")
+    @Operation(summary = "Movies count",
+            description = "This gives a count of all the movies")
+    public Integer getMoviesCount() {
+        return movies.size();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/create-movie")
+    @Operation(summary = "Create Movie",
+            description = "This API will create a movie")
+    public Response createMovie(Movie newMovie) {
+        movies.add(newMovie);
+        return Response.ok(movies).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/fetch-external/{searchValue}")
+    public Response fetchExternal(@PathParam("searchValue") String searchValue){
+        List<TvMazeSeries> series = tvMazeApi.search(searchValue);
+        return Response.ok(series).build();
+    }
+}
