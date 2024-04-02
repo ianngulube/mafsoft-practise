af5f8e413d1dd757b1dbaf0f7fae5175cfaf74a1
Ian Ngulube <ngulubeian94@gmail.com>
2024-04-02 09:12:51 +0200

    resources:refactor:cleanup

--- src/main/java/za/co/mafsoft/fundamentals/resources/MoviesResource.java
+++ src/main/java/za/co/mafsoft/fundamentals/resources/MoviesResource.java
@@ -1,8 +1,17 @@
 package za.co.mafsoft.fundamentals.resources;
 
-import jakarta.ws.rs.*;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -12,9 +21,12 @@ import za.co.mafsoft.fundamentals.model.TvMazeSeries;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Path("/movies")
 @Tag(name = "Movies Resource", description = "API to create, read, update and delete movies collection")
+@Slf4j
+@ApplicationScoped
 public class MoviesResource {
 
     @RestClient
@@ -53,8 +65,27 @@ public class MoviesResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/fetch-external/{searchValue}")
-    public Response fetchExternal(@PathParam("searchValue") String searchValue){
-        List<TvMazeSeries> series = tvMazeApi.search(searchValue);
+    public Response fetchExternal(@PathParam("searchValue") String searchValue) {
+        List<TvMazeSeries> series = getSeries(searchValue);
         return Response.ok(series).build();
     }
+
+    private List<TvMazeSeries> fallBackMessage() {
+        log.info("Fallback message ########");
+        return new ArrayList<>();
+    }
+    @Timeout(200)
+    @Fallback(fallbackMethod = "fallBackMessage")
+    private List<TvMazeSeries> getSeries(final String searchValue) {
+
+        try {
+            log.info("Sleeping ****************");
+            TimeUnit.MILLISECONDS.sleep(5000);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return tvMazeApi.search(searchValue);
+    }
+
 }
